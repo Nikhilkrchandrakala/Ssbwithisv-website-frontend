97a3b3511c16e150232070f10f8e3416ccaeae5c
Ayush Tripathi <isvclub2021@gmail.com>
2026-06-17 20:50:45 +0530

    deploy: automated push before VPS deployment

---
+++ read_nginx_config.js
@@ -0,0 +1,25 @@
+const { Client } = require('ssh2');
+
+const conn = new Client();
+conn.on('ready', () => {
+  console.log('Client :: ready');
+  conn.exec('cat /etc/nginx/sites-enabled/* /etc/nginx/conf.d/* /etc/nginx/nginx.conf', (err, stream) => {
+    if (err) throw err;
+    let stdout = '';
+    stream.on('close', (code, signal) => {
+      console.log('--- REMOTE NGINX CONFIG CONTENTS ---');
+      console.log(stdout);
+      console.log('------------------------------------');
+      conn.end();
+    }).on('data', (data) => {
+      stdout += data;
+    });
+  });
+}).on('error', (err) => {
+  console.error('Connection Error:', err);
+}).connect({
+  host: '88.222.214.155',
+  port: 22,
+  username: 'root',
+  password: 'Joint@3services'
+});
