ffd5369f9d1294790033f7f699703e7d4f590f94
Ayush Tripathi <isvclub2021@gmail.com>
2026-06-27 11:20:08 +0530

    perf: defer Razorpay and Zoho PageSense scripts to prevent parser-blocking

--- src/pages/LoadingScreen.jsx
+++ src/pages/LoadingScreen.jsx
@@ -1,42 +1,8 @@
-import { useEffect } from "react";
+import React from "react";
 import "../style/LoadingScreen.css";
-import axios from "axios";
-import { useUserProfileQuery } from "../redux/api";
-// import logo from "../assets/logo.png"; // 👉 apna logo yahan daalo
-
 export default function LoadingScreen() {
-
-    // useEffect(()
-
-    // const { data } = useUserProfileQuery()
-
-    const {
-        data,
-        error,
-        isLoading,
-        isSuccess,
-        isError
-    } = useUserProfileQuery();
-
-    console.log(data)
-
-
-    useEffect(() => {
-        if (isSuccess && data?.status === "ok") {
-            console.log("User authenticated", data.user);
-        }
-
-        if (isError) {
-            console.log("Token expired or invalid");
-            localStorage.removeItem("authToken");
-            sessionStorage.removeItem("authToken");
-            // window.location.href = "/login";
-        }
-
-    }, [isSuccess, isError, data]);
-
-
     return (
+
 
         <div className="loading-root">
             <div className="loading-content">
